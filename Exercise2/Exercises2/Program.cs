b74057d50035f58b7d1e5aec204f282ae09b7859
Saeed.Sabzi <saeed.sabzi.e@gmail.com>
2022-05-23 09:26:26 +0430

    Exe2 added

---
+++ Exercise2/Exercises2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercises2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------");
+            int i, j = 0;
+            for (i = 0; i <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    j += i ;
+                }
+            }
+            Console.WriteLine(j);
+        }
+    }
+}
--- Triangle Numbers/ConsoleApp-Task1/Program.cs
+++ Triangle Numbers/ConsoleApp-Task1/Program.cs
@@ -6,32 +6,54 @@ namespace ConsoleApp_Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many line do you want to set numbers?");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("-----------------------------------------");
-            Console.ForegroundColor = ConsoleColor.Green;
-            int i = 1;
-            int j = 1;
-            while (i <= num)
+            //Console.WriteLine("How many line do you want to set numbers?");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("-----------------------------------------");
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //int i = 1;
+            //int j = 1;
+            //while (i <= num)
+            //{
+            //    for (int Null = 0; Null < num - i; Null++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    for (int k = j - 2; k >= 1; k--)
+            //    {
+            //        Console.Write(k);
+            //    }
+            //    for (int Null = 0; Null < num - i; Null++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    i++;
+            //    Console.WriteLine();
+            //}
+            int rows = 5;
+            for (int i = 0; i <= rows; i++)
             {
-                for (int Null = 0; Null < num - i; Null++)
+
+                for (int j = 0; j < rows - i; j++)
                 {
                     Console.Write(" ");
                 }
-                for (j = 1; j <= i; j++)
+
+                for (int h = 1; h <= i; h++)
                 {
-                    Console.Write(j);
+                    Console.Write($"{h}");
                 }
-                for (int k = j - 2; k >= 1; k--)
+
+                for (int k = i - 1; k >= 1; k--)
                 {
-                    Console.Write(k);
+                    Console.Write($"{k}");
                 }
-                for (int Null = 0; Null < num - i; Null++)
-                {
-                    Console.Write(" ");
-                }
-                i++;
-                Console.WriteLine();
+
+                Console.WriteLine("");
+
             }
             Console.ReadKey();
         }

a2c0764a6aef11cc777c7c2d4370aa322b80d7e2
Saeed.Sabzi <saeed.sabzi.e@gmail.com>
2022-05-28 21:55:10 +0430

    Excercise3 Added

---
+++ Excercise3/Excercise3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Excercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //دریافت متن از کاربر
+            string a = Console.ReadLine().ToLower();
+            string b = Console.ReadLine().ToLower();
+            //جدا کردن کلمات وارد شده از هم
+            string[] S1 = a.Split(" ");
+            string[] S2 = b.Split(" ");
+            Console.WriteLine("=====================================================================");
+
+            //بررسی تک تک کلمات با همدیگر
+            foreach (var item in S1)
+            {
+                for (int i = 0; i < S2.Length; i++)
+                {
+                    if (item == S2[i])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"\"{item}\" and \"{S2[i]}\" are same in both string 1 and string 2.");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"\"{item}\" and \"{S2[i]}\" aren't same in both string 1 and string 2!!! ");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

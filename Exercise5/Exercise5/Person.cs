23716df1c1b437a11ce077ab45af99997c2e9adf
Saeed.Sabzi <saeed.sabzi.e@gmail.com>
2022-06-03 01:30:21 +0430

    Exercise5 Added

---
+++ Exercise5/Exercise5/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    public class Person
+    {
+        public string Fullname { get; set; }
+        public int PerHour { get; set; }
+        public int Hours { get; set; }
+        public int DaysInMonth { get; set; }
+
+
+        public Person(string fullname, int perHour, int hours, int days)
+        {
+            Fullname = fullname;
+            PerHour = perHour;
+            Hours = hours;
+            DaysInMonth = days;
+        }
+        // Method
+        public int Result()
+        {
+            int result = Hours * PerHour * DaysInMonth;
+            return result;
+        }
+    }
+}
---
+++ Exercise5/Exercise5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            List<Person> persons = new List<Person>();
+
+            Console.Write("How many person's income, you want to calculate? ");
+            int personCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------");
+
+            while (i < personCount)
+            {
+                Console.Write("Enter Fullname: ");
+                string fullname = Console.ReadLine();
+                Console.Write("How many Hours? ");
+                int hours = Convert.ToInt32(Console.ReadLine());
+                Console.Write("How much per hour? ");
+                int perhour = Convert.ToInt32(Console.ReadLine());
+                Console.Write("How many days? ");
+                int days = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("--------------------");
+                Person obj = new Person(fullname, perhour, hours, days);
+                persons.Add(obj);
+                i++;
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            foreach (var item in persons)
+            {
+                Console.WriteLine($"Name: {item.Fullname}");
+                Console.WriteLine($"Hourly Income: {item.PerHour}");
+                Console.WriteLine($"Hours of per day: {item.Hours}");
+                Console.WriteLine($"Salary: {item.Result()}");
+                Console.WriteLine("===================");
+            }
+            Console.ReadKey();
+        }
+    }
+}

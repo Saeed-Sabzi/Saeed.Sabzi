5607de90eec5380aecdd21a872b9bf65a7e24987
Saeed Sabzi <saeed.sabzi.e@gmail.com>
2022-06-13 22:43:28 +0430

    Add Exercise7_RecursiveFunction

---
+++ Exercise7_RecursiveFunction/Exercise7_RecursiveFunction/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7_RecursiveFunction
+{
+    public class Class
+    {
+        
+        public static string ReverseWord(string word)
+        {
+            if (word.Length == 0)
+            {
+                return word;
+            }
+            return ReverseWord(word.Substring(1)) + word[0];
+        }
+
+
+    }
+}
---
+++ Exercise7_RecursiveFunction/Exercise7_RecursiveFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise7_RecursiveFunction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(Class.ReverseWord("Welcome"));
+
+            Console.ReadLine();
+        }
+    }
+}

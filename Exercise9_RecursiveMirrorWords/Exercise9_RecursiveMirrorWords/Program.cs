e53e0dc91b918be89a7e3627f17cd714565127d9
Saeed Sabzi <saeed.sabzi.e@gmail.com>
2022-06-16 23:00:11 +0430

    add Exercise9_RecursiveMirrorWords

---
+++ Exercise9_RecursiveMirrorWords/Exercise9_RecursiveMirrorWords/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise9_RecursiveMirrorWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input = Console.ReadLine().ToLower();
+            Console.WriteLine(Madam(Input));
+        }
+
+
+        public static bool Madam(string input)
+        {
+            if (input.Length == 1)
+            {
+                return true;
+            }
+            else if (input.Length == 2 && input[0] == input[1])
+            {
+                return true;
+            }
+            else
+            {
+                if (input[0] == input[input.Length - 1])
+                {
+                    return Madam(input.Substring(1, input.Length - 2));
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}

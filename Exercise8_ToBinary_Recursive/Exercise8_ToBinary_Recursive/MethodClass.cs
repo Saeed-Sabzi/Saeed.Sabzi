1b471cfde09a8dbbce8a0a31bf0e278305bfe500
Saeed Sabzi <saeed.sabzi.e@gmail.com>
2022-06-16 22:27:33 +0430

    add Exercise8_ToBinary_Recursive

---
+++ Exercise8_ToBinary_Recursive/Exercise8_ToBinary_Recursive/MethodClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8_ToBinary_Recursive
+{
+    public class MethodClass
+    {
+        public static long ToBinary(int number)
+        {
+            if (number / 2 == 0)
+            {
+                return number % 2;
+            }
+            return number % 2 + 10 * (ToBinary(number / 2));
+        }
+    }
+}
---
+++ Exercise8_ToBinary_Recursive/Exercise8_ToBinary_Recursive/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise8_ToBinary_Recursive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MethodClass.ToBinary(number));
+
+            Console.ReadKey();
+        }
+    }
+}
